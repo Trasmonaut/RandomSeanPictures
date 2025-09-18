d2ae6c25190900387543876bfb0097c3191f957f
Caleb <c.ramdath.26@gmail.com>
2025-09-18 18:19:50 -0400

    c

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function LoadImageFromFiles(){
     const imageElement = document.getElementById('mainImage');
-    const randomImageNumber = Math.floor(Math.random() * 6) + 1;
+    const randomImageNumber = Math.floor(Math.random() * 11) + 1;
 
     imageElement.src = `images/${randomImageNumber}.jpg`;   
     imageElement.alt = `Random Sean Picture ${randomImageNumber}`;
